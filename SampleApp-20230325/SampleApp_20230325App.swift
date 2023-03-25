00490c2b4e5be9d4adb392447b955620417f2a1b
Hideyuki Nanashima <jollyjoester@gmail.com>
2023-03-25 20:58:26 +0900

    Add project.

---
+++ SampleApp-20230325/SampleApp_20230325App.swift
@@ -0,0 +1,17 @@
+//
+//  SampleApp_20230325App.swift
+//  SampleApp-20230325
+//
+//  Created by Hideyuki Nanashima on 2023/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SampleApp_20230325App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
